d2bc0cf77c67214d9f4062447abc6817dca66053
Kom3ng <3262392248@qq.com>
2024-01-28 15:32:03 +0800

    fix: fix bugs

--- main.js
+++ main.js
@@ -1,80 +1,80 @@
 // ==UserScript==
 // @name         看过动画自动标记音乐
-// @namespace    http://tampermonkey.net/
-// @version      1
+// @namespace    http://astrack.icu/
+// @version      0.1.0
 // @description  看过动画自动标记音乐
 // @author       astrack
-// @match        *://chii.in/subject/*
-// @match        *://bangumi.tv/subject/*
-// @match        *://bgm.tv/subject/*
+// @include      /^http(s)?://(bgm.tv|bangumi.tv|chii.in)/subject/\d+$/
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
-(function() {
-    'use strict'
+'use strict';
 
-    document.addEventListener('click',(e) => {
-        const target = $(e.target)
-        const form = $('#collectBoxForm')
+function isMusic(e) {
+    const title = e.querySelector('span').innerText;
+    return title === '原声集' ||
+        title === '片头曲' ||
+        title === '片尾曲' ||
+        title === '插入歌';
+}
 
-        // [1]
-        if (
-            target.hasClass('inputBtn') &&
-            target.attr('type') === 'submit' &&
-            target.attr('name') === 'update' &&
-            $('#collect').is(':checked')
-        ) {
-            form.on('submit', () => false)
-            e.preventDefault();
+document.addEventListener('click', (e) => {
+    const target = $(e.target);
+    const form = $('#collectBoxForm');
 
-            // 防止重复点击
-            target.disable()
-            // 关联条目
-            const relation = document.querySelector('.subject_section ul.browserCoverMedium')
-            if (relation == null) return
-            const list = relation.querySelectorAll('li')
-            // [2] 获取gh
-            const submitGh = form.attr('action').split('?gh=').pop()
+    if (
+        target.hasClass('inputBtn') &&
+        target.attr('type') === 'submit' &&
+        target.attr('name') === 'update' &&
+        $('#collect').is(':checked')
+    ) {
+        form.on('submit', () => false);
+        e.preventDefault();
 
-            new Promise(async () => {
-                for (let i = 0; i < list.length; i++) {
-                    const title = list[i].querySelector('span').innerText
-                    // 找到音乐的第一个元素
-                    if (title !== '原声集' &&
-                        title !== '片头曲' &&
-                        title !== '片尾曲') {
-                        continue
-                    }
-                    // 收藏这一类的所有
-                    for (; i < list.length; i++) {
-                        // 判断是否是新一类
-                        if (list[i].classList.contains('sub')) break
+        // 防止重复点击
+        target.attr("disabled", "disabled");
+        target.attr('value','提交中...');
 
-                        const el = list[i]
-                        const href = el.querySelector('a').href;
-                        const subjectName = el.querySelector('.title').innerText;
+        // 关联条目
+        const relation = document.querySelector('.subject_section ul.browserCoverMedium');
+        if (relation == null) return;
+        const list = relation.querySelectorAll('li');
+        const submitGh = form.attr('action').split('?gh=').pop();
 
-                        // 收藏
-                        await fetch(
-                            `${href}/interest/update?gh=${submitGh}&ajax=1`,
-                            {
-                                headers: {
-                                    'Content-Type': 'application/x-www-form-urlencoded',
-                                },
-                                method: 'POST',
-                                body: 'referer=subject&interest=2&tags=&comment=&update=%E4%BF%9D%E5%AD%98',
-                            },
-                        ).then(() => console.log(`收藏 ${subjectName}`))
-                    }
+        new Promise(async () => {
+            for (let i = 0; i < list.length; i++) {
+                // 找到第一个元素
+                if (!isMusic(list[i])) {
+                    continue;
                 }
-                // [3] 提交原来的表单
-                $.post(form.attr('action'), form.serialize()).done(() =>
-                    location.reload()
-                );
-            }).then(()=>{})
-        }
-    })
-})()
 
-// [1] [2] [3] 引用自https://bgm.tv/dev/app/461
+                // 收藏
+                do {
+                    const el = list[i];
+                    const href = el.querySelector('a').href;
+                    const subjectName = el.querySelector('.title').innerText;
+
+                    await fetch(
+                        `${href}/interest/update?gh=${submitGh}&ajax=1`,
+                        {
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                            },
+                            method: 'POST',
+                            body: 'referer=subject&interest=2&tags=&comment=&update=%E4%BF%9D%E5%AD%98',
+                        },
+                    ).then(() => console.log(`收藏 ${subjectName}`)).catch(e => console.log(e));
+                    i++;
+                } while (i < list.length && !list[i].classList.contains('sep'));
+                // 将最后一次循环增加的索引向前挪1
+                i--;
+            }
+            // 提交原来的表单
+            $.post(form.attr('action'), form.serialize()).done(() =>
+                location.reload()
+            );
+        }).then(() => {
+        });
+    }
+});
